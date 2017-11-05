36b9e7d3fc12318ca130d2afd90f5f57e46640be
Christophe Cauet <ccauet@point-8.de>
2017-11-05 10:49:23 +0100

    Add CLI and read command

---
+++ ledgertools/cli.py
@@ -0,0 +1,35 @@
+import json
+import click
+import pickle
+
+from ledgertools.read import read_file
+from ledgertools.version import __version__
+
+@click.group(help='CLI tools for working with ledger')
+@click.version_option(version=__version__, prog_name='Ledger Tools')
+def cli():
+    pass
+
+
+@cli.command(help='Import ledger style file')
+@click.option('-f', '--file', 'in_file', help='Input file name', prompt='Input file name')
+@click.option('-n', '--name', default='transactions.json', help='Output file name')
+@click.option('-p', '--pickle', 'as_pickle', is_flag=True, help='Output as pickle file')
+def read(in_file, name, as_pickle):
+    click.secho(f'Reading input file: {in_file}', fg='green')
+    transactions = read_file(in_file)
+
+    if as_pickle:
+        name = name.replace('json', 'pkl')
+        with open(name, 'wb') as out_file:
+            # Pickle the 'data' dictionary using the highest protocol available.
+            pickle.dump(transactions, out_file, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(name, 'w', encoding='utf-8') as out_file:
+            json.dump(transactions, out_file, sort_keys=True, indent=4, ensure_ascii=False)
+
+    click.secho(f'Saving output to: {name}', fg='green')
+
+
+def main():
+    cli()
---
+++ ledgertools/read.py
@@ -0,0 +1,85 @@
+import math
+import datetime
+
+from tqdm import tqdm
+
+
+def get_date(s):
+    return datetime.datetime.strptime(s, '%Y/%m/%d').date().isoformat()
+
+
+def get_transactions(group):
+    transactions = []
+    for t in group[1:]:
+        t = [x for x in t.split('  ') if x]
+
+        account = t[0]
+        value = float(t[1].split()[0].replace(',', '.'))
+        date = [x.replace(';', '').replace('date:', '').strip()
+                for x in t[2:] if x not in [';']]
+
+        if date:
+            try:
+                date = get_date(date[0]).isoformat()
+            except Exception as e:
+                date = get_date(group[0].split()[0])
+        else:
+            date = get_date(group[0].split()[0])
+
+        transactions.append({
+            'account': account,
+            'date': date,
+            'value': value
+        })
+    return transactions
+
+
+def check_sum(doc, abs_tol=0.001):
+    return math.isclose(
+        sum([t['value'] for t in doc['transactions']]), 0, abs_tol=abs_tol)
+
+
+def parse(group):
+    doc = {}
+
+    header_line = group[0].split()
+    
+    # get group date
+    doc['group_date'] = get_date(header_line[0])
+    # get subject
+    # remove everything except group subject
+    # TODO: implement parsing of transaction status and invoice number
+    doc['subject'] = ' '.join([x for x in header_line[1:] if not x in ['*', '!'] and not x.startswith('(')])
+    doc['transactions'] = get_transactions(group)
+
+    # check total sum of all transactions is equal zero
+    if not check_sum(doc):
+        raise ArithmeticError('Transactions do not sum to zero', group)
+    return doc
+
+
+def read_file(in_file):
+    with open(in_file, 'r') as in_file:
+        lines = in_file.readlines()
+
+    # remove newline at EOL
+    # remove comments
+    # remove global definition of year
+    lines = [l.replace('\n', '') for l in lines if not l[0] in [';', 'Y']]
+
+    # get transaction groups
+    groups = []
+    group = []
+    for l in lines:
+        if not l:
+            groups.append(group)
+            group = []
+        else:
+            group.append(l)
+    groups = [g for g in groups if g]
+
+    transactions = []
+    for group in tqdm(groups):
+        transactions.append(parse(group))
+
+    return transactions
